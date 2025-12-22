db01d3bad29c50d275701eaac95729e7a8d11d68
VoAnhBen <benva.ce190709@gmail.com>
2025-12-22 17:41:19 +0700

    update

--- src/todo.component.tsx
+++ src/todo.component.tsx
@@ -5,15 +5,31 @@ interface IProps {
 
 const TodoComponent = (props: IProps) => {
     const { name } = props
+    const handleClick = () => {
+        alert("hi")
+    }
+    const todos = ["todo 1", "todo 2", "todo 3", "todo 4"]
     return (
         <div>
             <div>
                 {name}
-
             </div>
-            <input>
+            <input type="text" onChange={(event) => {
+                console.log(event.target.value)
+            }}>
             </input>
-            <button>Input</button>
+            <button onClick={() => {
+                handleClick()
+            }}>Input</button>
+            <ul>
+                {todos.map(it => {
+                    return (
+                        <li>
+                            {it}
+                        </li>
+                    )
+                })}
+            </ul>
         </div>
     )
 }
