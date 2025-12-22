fe95bcb39ba78815fdea11f9ca17d038f15973fd
VoAnhBen <benva.ce190709@gmail.com>
2025-12-22 23:55:13 +0700

    update

--- src/todo.component.tsx
+++ src/todo.component.tsx
@@ -1,35 +1,47 @@
+import { useState } from "react";
 
 interface IProps {
     name: string;
 }
-
+let cnt = 1;
 const TodoComponent = (props: IProps) => {
     const { name } = props
+    const [fullName, setFullName] = useState("");
     const handleClick = () => {
-        alert("hi")
+        setList([...list, todos])
+
+    }
+    const [list, setList] = useState(["todo 4", "todo 2", "todo 3", "todo 4"])
+    const increase = () => {
+        cnt = cnt + 1;
+
     }
     const todos = ["todo 1", "todo 2", "todo 3", "todo 4"]
     return (
         <div>
+            <button onClick={() => {
+                increase()
+            }}>Increase</button>
+            <div>{cnt}</div>
+            <div>{fullName}</div>
             <div>
                 {name}
             </div>
-            <input type="text" onChange={(event) => {
-                console.log(event.target.value)
+            <input value={todos} type="text" onChange={(event) => {
+                setFullName(event.target.value)
             }}>
             </input>
             <button onClick={() => {
                 handleClick()
             }}>Input</button>
             <ul>
-                {todos.map(it => {
+                {list.map((it, index) => {
                     return (
-                        <li>
-                            {it}
-                        </li>
+                        <li key={index}>{it}</li>
                     )
                 })}
             </ul>
+            <span>{cnt}</span>
         </div>
     )
 }
