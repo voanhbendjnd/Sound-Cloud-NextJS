40e472592061044d3c648dfb9fa60412d560971b
VoAnhBen <benva.ce190709@gmail.com>
2025-12-29 21:04:24 +0700

    update

--- src/components/users/users.table.tsx
+++ src/components/users/users.table.tsx
@@ -1,11 +1,35 @@
 import { useEffect, useState } from 'react'
-import '../../styles/users.css'
+// import '../../styles/users.css'
+import type { ColumnsType } from 'antd/es/table';
+import Table from 'antd/es/table';
 interface IUsers {
     id: number;
     name: string,
     email: string,
 }
+
 const UsersTable = () => {
+    const columns: ColumnsType<IUsers> = [
+        {
+            title: 'ID',
+            dataIndex: 'id',
+            render: (value, record) => {
+                return (
+                    <a>
+                        {record.id}
+                    </a>
+                )
+            }
+        },
+        {
+            title: "Name",
+            dataIndex: 'name'
+        },
+        {
+            title: "Email",
+            dataIndex: "email"
+        }
+    ]
     const [listUsers, setListUsers] = useState<IUsers[]>([]);
     const getData = async () => {
         const res = await fetch("http://localhost:8080/api/v1/users", {
@@ -23,6 +47,7 @@ const UsersTable = () => {
         setListUsers(d.result)
     }
     useEffect(() => {
+        // eslint-disable-next-line react-hooks/set-state-in-effect
         getData();
     }, [])
     return (
@@ -30,7 +55,13 @@ const UsersTable = () => {
             <div>
                 User Table
             </div>
-            <table>
+            <Table
+                columns={columns}
+                dataSource={listUsers}
+            >
+
+            </Table>
+            {/* <table>
                 <thead>
                     <tr>
                         <td>ID</td>
@@ -53,7 +84,7 @@ const UsersTable = () => {
                     }
                 </tbody>
 
-            </table>
+            </table> */}
         </div>
     )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,64 @@
-import { StrictMode } from 'react'
+import React, { StrictMode, useState } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { createBrowserRouter, Link, Outlet, RouterProvider } from 'react-router-dom';
 import UsersPage from './screens/users.page.tsx';
+import './App.scss'
+import { HomeOutlined, WindowsOutlined } from '@ant-design/icons';
+import type { MenuProps } from 'antd';
+import { Menu } from 'antd';
 
+type MenuItem = Required<MenuProps>['items'][number];
+
+const items: MenuItem[] = [
+  {
+    label: <Link to='/'>Home</Link>,
+    key: 'home',
+    icon: <HomeOutlined />,
+  },
+  {
+    label: <Link to='/users'>User Management</Link>,
+    key: 'management_user',
+    icon: <WindowsOutlined />,
+  }
+];
+// eslint-disable-next-line react-refresh/only-export-components
+const Header = () => {
+  const [current, setCurrent] = useState('home');
+
+  const onClick: MenuProps['onClick'] = (e) => {
+    console.log('click ', e);
+    setCurrent(e.key);
+  };
+
+  return (
+    <Menu onClick={onClick} selectedKeys={[current]} mode="horizontal" items={items} />
+  )
+};
+// eslint-disable-next-line react-refresh/only-export-components
+const LayoutAdmin = () => {
+  return (
+    <div>
+      <Header />
+      <Outlet />
+    </div>
+  )
+}
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <App></App>
-  },
-  {
-    path: "/users",
-    element: <UsersPage />
+    element: <LayoutAdmin />,
+    children: [
+      {
+        index: true,
+        element: <App />
+      },
+      {
+        path: "users",
+        element: <UsersPage />
+
+      }
+    ]
   }
 ])
 createRoot(document.getElementById('root')!).render(
